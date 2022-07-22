0578be2e865a63ad4b565baf3a52eaf4b3c3b98e
Maksim Rasporskyi <mraspors@lab1r1s8.42abudhabi.ae>
2022-07-22 23:28:52 +0400

    some improvements

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mraspors <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 02:25:11 by mraspors          #+#    #+#             */
-/*   Updated: 2022/07/22 23:09:24 by mraspors         ###   ########.fr       */
+/*   Updated: 2022/07/22 23:28:28 by mraspors         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ int main(int argc, char **argv, char **env)
 	char	*line;
 	char	**args;
 	int i = 0;
-
+	char *s = getcwd(NULL, 0);
+	strcat(s, "$ ");
 	env_list = NULL;
 	init_env_list(&env_list, env);
 	while (1)
 	{
-		line = readline("");
+		line = readline(s);
 		args = ft_split(line, ' ');
 		ft_echo(args);
 	}
