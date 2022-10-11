d77e5da34872dc884546e1177cdb44cbfb49bfe3
Maksim Rasporskyi <mraspors@lab3r5s5.42abudhabi.ae>
2022-10-11 06:36:14 +0400

    echo -n flag added

--- src/execution/builtins1.c
+++ src/execution/builtins1.c
@@ -6,7 +6,7 @@
 /*   By: mraspors <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 20:31:15 by mrizk             #+#    #+#             */
-/*   Updated: 2022/09/12 09:42:24 by mraspors         ###   ########.fr       */
+/*   Updated: 2022/10/11 06:35:42 by mraspors         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,21 @@
 int	ft_echo(t_cmd *cmd)
 {
 	int	i;
+	int	flag;
 
+	flag = 0;
 	i = 1;
 	if (ft_strcmp("echo", cmd->args[0]) == 0)
 	{
+		if (ft_strcmp(cmd->args[1], "-n") == 0)
+		{
+			flag = 1;
+			i = 2;
+		}	
 		while (cmd->args[i] != NULL)
 			printf("%s ", cmd->args[i++]);
-		printf("\n");
+		if (flag == 0)
+			printf("\n");
 		return (0);
 	}
 	return (1);
