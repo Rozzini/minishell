a21182b2d9139cad811929aa120bf1b416fe183a
Maksim Rasporskyi <mraspors@lab1r1s8.42abudhabi.ae>
2022-11-03 06:02:34 +0400

    CTRL+C CTRL+/ signals hadled, almost

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: mraspors <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 04:22:10 by mraspors          #+#    #+#             */
-/*   Updated: 2022/11/03 04:49:11 by mraspors         ###   ########.fr       */
+/*   Updated: 2022/11/03 05:54:27 by mraspors         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ void	new_line_sig(int sig)
 	{
 		sig_exit_code(sig);
 		ioctl(STDIN_FILENO, TIOCSTI, "\n");
-		ft_putchar_fd('\n', 1);
 		rl_replace_line("", 1);
 		rl_on_new_line();
 	}
