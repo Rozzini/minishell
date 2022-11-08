784577962de11c6e05818c461f9d6aac232947d3
zozi macbook <y.700@live.com>
2022-11-08 10:07:06 +0400

    pipe.c

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -6,60 +6,77 @@
 /*   By: alalmazr <alalmazr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 20:37:03 by mraspors          #+#    #+#             */
-/*   Updated: 2022/11/06 21:05:39 by alalmazr         ###   ########.fr       */
+/*   Updated: 2022/11/08 10:03:46 by alalmazr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
 
-int make_baby_pipe(int *prev_new_fd, t_cmd *cmd, t_env **env)
+void	ft_peepeeing(int fd_in, t_cmd *cmd)
 {
+	//-----update-------------------
+	if (cmd->next != NULL)
+	{
+		if (fd_in != 0)
+		{
+			dup2(fd_in, STDIN_FILENO);
+			close(fd_in);
+		}
+		if (cmd->output->fd != 1)
+		{
+			dup2(cmd->output->fd, STDOUT_FILENO);
+			close(cmd->output->fd);
+		}
+	}
+	else
+	{
+		dup2(fd_in, STDIN_FILENO);
+		close(fd_in);
+	}
+	//-----update
+}
+
+int make_baby_pipe(int fd_in, t_cmd *cmd, t_env **env)
+{
+	//-----update-------------------
 	pid_t pid;
 
-	if (try_parent_builtins(cmd, env) == 1)
-		return (0);
 	pid = fork();
 	if (pid == 0)
 	{
-		if (prev_new_fd[0] != 0)
-		{
-			dup2(prev_new_fd[0], STDIN_FILENO);
-			close(prev_new_fd[0]);
-		}
-		if (prev_new_fd[1] != 1)
-		{
-			dup2(prev_new_fd[1], STDOUT_FILENO);
-			close(prev_new_fd[1]);
-		}
+		if (try_parent_builtins(cmd, env) == 1)
+			exit (g_signal);
+		ft_peepeeing(cmd);
 		if (cmd->input || cmd->output)
-			return (exec_redir(cmd, env));
+			exec_redir(cmd, env);
 		else
 			ft_execs(cmd, env);
+		exit(g_signal);
 	}
 	return (pid);
+	//-----update-------------------
 }
 
 void exec_pipes(t_cmd *cmd, t_env **env)
 {
-	int fd[2]; // fd[0]->read fd[1]->write
-	int prev_new_fd[2]; //prev_new[0] is the original and prev_new[1] is the write end from fd[1]
-	pid_t pid;
+	//-----update-------------------
+	int		fd[2]; // fd[0]->read fd[1]->write
+	pid_t	pid;
+	int		fd_in;
 
-	//first process  getS input from the stdin->0
-	prev_new_fd[0] = STDIN_FILENO;
-	//make all child processes except for last which we will redirect to stdout
-	// printf("	no of cmds:%d\n", n_cmd);
-	while (cmd->next != NULL)
+	// first process  getS input from the stdin->0
+	fd_in = STDIN_FILENO;
+	while (cmd != NULL)
 	{
 		if (pipe(fd) == -1)
 		{
 			printf("pipe fked\n");
 		}
 		// f[1]->write end of the pipe
-		// carry `prev` from the prev iteration.
-		prev_new_fd[1] = fd[1]; // ***
-		pid = make_baby_pipe(prev_new_fd, cmd, env);
+		//when we exec->output of command goes to write end of pipe
+		cmd->output->fd = fd[1]; // ***
+		pid = make_baby_pipe(fd_in, cmd, env);
 		if (pid == -1)
 		{
 			printf("fork error");
@@ -68,21 +85,8 @@ void exec_pipes(t_cmd *cmd, t_env **env)
 		//no need for write end bcz child process will use it
 		close(fd[1]);
 		//save fd[0] to give it to next child
-		prev_new_fd[0] = fd[0];
+		fd_in = fd[0];
 		cmd = cmd->next;
 	}
-	//last cmd on pipe: set stdin be the read end of the prev pipe
-	//and output to the original STDOUT
-	// if (prev_new_fd[0] != 0)
-		dup2(prev_new_fd[0], STDIN_FILENO);
-	close(prev_new_fd[0]);
-	if (cmd->output || cmd->input)
-		exec_redir(cmd, env);
-	else
-	{
-		if (try_parent_builtins(cmd, env) == 1)
-			return ;
-		ft_execs(cmd, env);
-	}
-	return ;
+	//-----update-------------------
 }
