375b9eb5d51d2ac1876239154d126b92d87a8e66
Maksim Rasporskyi <mraspors@lab1r1s8.42abudhabi.ae>
2022-11-08 09:55:55 +0400

    comments removed

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -6,7 +6,7 @@
 /*   By: mraspors <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 20:37:03 by mraspors          #+#    #+#             */
-/*   Updated: 2022/11/08 09:54:19 by mraspors         ###   ########.fr       */
+/*   Updated: 2022/11/08 09:55:45 by mraspors         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,9 @@ int make_baby_pipe(int *prev_new_fd, t_cmd *cmd, t_env **env)
 
 void exec_pipes(t_cmd *cmd, t_env **env)
 {
-	int fd[2]; // fd[0]->read fd[1]->write
-	int prev_new_fd[2]; //prev_new[0] is the original and prev_new[1] is the write end from fd[1]
-	pid_t pid;
+	int fd[2];
+	int prev_new_fd[2];
 
-	//first process  getS input from the stdin->0
 	prev_new_fd[0] = STDIN_FILENO;
 	while (cmd != NULL)
 	{
@@ -60,9 +58,7 @@ void exec_pipes(t_cmd *cmd, t_env **env)
 		{
 			printf("pipe fked\n");
 		}
-		// f[1]->write end of the pipe
-		// carry `prev` from the prev iteration.
-		prev_new_fd[1] = fd[1]; // ***
+		prev_new_fd[1] = fd[1];
 		pid = make_baby_pipe(prev_new_fd, cmd, env);
 		wait(0);
 		if (pid == -1)
@@ -71,7 +67,6 @@ void exec_pipes(t_cmd *cmd, t_env **env)
 			return ;
 		}
 		close(fd[1]);
-		//save fd[0] to give it to next child
 		prev_new_fd[0] = fd[0];
 		cmd = cmd->next;
 	}
@@ -79,29 +74,3 @@ void exec_pipes(t_cmd *cmd, t_env **env)
 	close(fd[1]);
 	wait(0);
 }
-
-// void	exec_pipes(t_cmd *cmd, t_env **env)
-// {
-// 	int	fd[2];
-// 	pid_t pid;
-
-//     if (cmd->next != NULL)
-//     {
-//         if (pipe(fd) != 0)
-//             printf("Failed to create pipe");
-//         if ((pid = fork()) < 0)
-//             printf("Failed to fork");
-//         if (pid == 0)
-//         {
-// 			dup2(fd[1], 1);
-//     		close(fd[0]);
-//     		close(fd[1]);
-//    			exec_pipes(cmd, env);
-//         }
-//         /* Fix standard input to read end of pipe */
-//         dup2(fd[0], 0);
-//         close(fd[0]);
-//         close(fd[1]);
-//     }
-//     ft_execs(cmd, env);
-// }
